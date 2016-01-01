ecac8235b305db6785969e6383241cc1ac74f7cf
arnabsarker <arnabs@seas.upenn.edu>
2016-01-01 12:43:45 -0500

    Create QType.java

---
+++ QType.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ * @author arnabsarker
+ *Stores the six categories of questions and a win category
+ */
+public enum QType {
+   ART, ENTERTAINMENT, GEOGRAPHY, HISTORY, SCIENCE, SPORTS, WIN;
+}
